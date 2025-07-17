1d757b4ec4da38f974f39b22f8d724ce6dcd2878
TahjP <tplummer@algomau.ca>
2025-07-17 00:17:43 +0000

    created permissions table and added and amin user

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -13,7 +13,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarPublic" aria-controls="navbarPublic" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-  
+
     <div class="collapse navbar-collapse" id="navbarPublic">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
